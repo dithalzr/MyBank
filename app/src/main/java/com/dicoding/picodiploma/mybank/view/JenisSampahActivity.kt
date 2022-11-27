8ccd7c492fbe3c4db5503831e623668e7f7d81f5
dithalzr <100126140+dithalzr@users.noreply.github.com>
2022-11-28 00:11:11 +0700

    first change

--- app/src/main/java/com/dicoding/picodiploma/mybank/view/JenisSampahActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/mybank/view/JenisSampahActivity.kt
@@ -31,13 +31,13 @@ class JenisSampahActivity : AppCompatActivity() {
 
     @SuppressLint("ObsoleteSdkInt")
     private fun setStatusBar() {
-        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
         if (Build.VERSION.SDK_INT >= 21) {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
             window.statusBarColor = Color.TRANSPARENT
-        }*/
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
